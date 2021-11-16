269017aa2917d0da0f96234420ed6f2b8e2a7306
Martin Johnsson <maartinjohnsson@gmail.com>
2021-11-16 23:07:43 +0100

    Created inital html and Javascript

---
+++ main.js
@@ -0,0 +1,13 @@
+darkMode.addEventListener("click", () => {
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "limegreen";
+});
+lightMode.addEventListener("click", () => {
+  document.body.style.backgroundColor = "white";
+  document.body.style.color = "black";
+});
+// Question  - sant eller falskt 3x
+
+// Question - tre svarsalternativ 3x
+
+//Question - 1 fråga med minst 4 checkboxar
