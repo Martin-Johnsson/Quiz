ab043a6fc0f96e98ec9483662df73316ec966f7f
Martin Johnsson <maartinjohnsson@gmail.com>
2021-11-24 13:38:13 +0100

    Added Javascript

--- main.js
+++ main.js
@@ -1,10 +1,53 @@
-darkMode.addEventListener("click", () => {
-  document.body.style.backgroundColor = "black";
-  document.body.style.color = "limegreen";
+// Switch color button
+switchColorThemeBtn.addEventListener("click", () => {
+  if (document.body.style.background === "white") {
+    document.body.style.background = "black";
+    document.body.style.color = "limegreen";
+    switchColorThemeBtn.innerHTML = `Light Mode`;
+  } else {
+    document.body.style.background = "white";
+    document.body.style.color = "black";
+  }
 });
-lightMode.addEventListener("click", () => {
-  document.body.style.backgroundColor = "white";
-  document.body.style.color = "black";
+let score = 0;
+//Submita poängen
+submitScoreBtn.addEventListener("click", () => {
+  let score = 0;
+  if (document.getElementById("question1Alt1").checked) {
+    score++;
+  }
+  if (document.getElementById("question2Alt2").checked) {
+    score++;
+  }
+  if (document.getElementById("question3Alt1").checked) {
+    score++;
+  }
+  if (document.getElementById("question4Alt2").checked) {
+    score++;
+  }
+  if (document.getElementById("question5Alt3").checked) {
+    score++;
+  }
+  if (document.getElementById("question6Alt2").checked) {
+    score++;
+  }
+  if (
+    document.getElementById("question7Alt1").checked &&
+    document.getElementById("question7Alt2").checked
+  ) {
+    score++;
+  }
+  let finalResult = document.createElement("p");
+  finalResult.textContent = `Du har svarat rätt på ${score} av 7 frågor!`;
+  resultDiv.appendChild(finalResult);
+  if (score === 7) {
+    finalResult.style.color = "darkgreen";
+  } else if (score > 4) {
+    finalResult.style.color = "orange";
+  } else {
+    finalResult.style.color = "red";
+  }
+  submitScoreBtn.remove();
 });
 // Question  - sant eller falskt 3x
 
